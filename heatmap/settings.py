d39e6e65af4a028ceb4a87a64347b33734086d48
lilianluca <lilianluca2118@gmail.com>
2023-11-19 17:10:29 +0100

    moving credentials to dotenv

--- heatmap/settings.py
+++ heatmap/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+# Load environment variables from .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,11 +90,11 @@ WSGI_APPLICATION = "heatmap.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'heatdb',
-        'USER': 'root',
-        'PASSWORD': 'mysqlbrk1',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT")
     }
 }
 
