1f52f8cf9c673a03b1a3b3e3df25a7e7f3924d3a
lilianluca <lilianluca2118@gmail.com>
2023-12-01 10:33:51 +0100

    Added layers types

--- frontend/src/custom/hooks/useMap.js
+++ frontend/src/custom/hooks/useMap.js
@@ -9,15 +9,86 @@ const useMap = (mapContainer, MAPYCZ_API_KEY, heatData, isLoading) => {
       [50.772245567257514, 15.0744030469653],
       16
     );
-    L.tileLayer(
-      `https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${MAPYCZ_API_KEY}`,
-      {
-        minZoom: 0,
-        maxZoom: 19,
-        attribution:
-          '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
-      }
-    ).addTo(map);
+
+    /*
+    We store all our tile layers in an object, because we will
+    need to pass that to the layers switching map control.
+    */
+    const tileLayers = {
+      Basic: L.tileLayer(
+        `https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${MAPYCZ_API_KEY}`,
+        {
+          minZoom: 0,
+          maxZoom: 19,
+          attribution:
+            '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+        }
+      ),
+      Outdoor: L.tileLayer(
+        `https://api.mapy.cz/v1/maptiles/outdoor/256/{z}/{x}/{y}?apikey=${MAPYCZ_API_KEY}`,
+        {
+          minZoom: 0,
+          maxZoom: 19,
+          attribution:
+            '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+        }
+      ),
+      Winter: L.tileLayer(
+        `https://api.mapy.cz/v1/maptiles/winter/256/{z}/{x}/{y}?apikey=${MAPYCZ_API_KEY}`,
+        {
+          minZoom: 0,
+          maxZoom: 19,
+          attribution:
+            '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+        }
+      ),
+      Aerial: L.tileLayer(
+        `https://api.mapy.cz/v1/maptiles/aerial/256/{z}/{x}/{y}?apikey=${MAPYCZ_API_KEY}`,
+        {
+          minZoom: 0,
+          maxZoom: 19,
+          attribution:
+            '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+        }
+      ),
+    };
+
+    // Then we add the first raster tile layer to the map.
+    tileLayers["Basic"].addTo(map);
+
+    // Leaflet has a built-in map control for switching layers.
+    L.control.layers(tileLayers).addTo(map);
+
+    // Logo Mapy.cz
+    const LogoControl = L.Control.extend({
+      options: {
+        position: "bottomleft",
+      },
+
+      onAdd: function (map) {
+        const container = L.DomUtil.create("div");
+        const link = L.DomUtil.create("a", "", container);
+
+        link.setAttribute("href", "http://mapy.cz/");
+        link.setAttribute("target", "_blank");
+        link.innerHTML = '<img src="https://api.mapy.cz/img/api/logo.svg" />';
+        L.DomEvent.disableClickPropagation(link);
+
+        return container;
+      },
+    });
+
+    // finally we add our LogoControl to the map
+    new LogoControl().addTo(map);
+    // L.tileLayer(
+    //   `https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${MAPYCZ_API_KEY}`,
+    //   {
+    //     minZoom: 0,
+    //     maxZoom: 19,
+    //     attribution:
+    //       '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+    //   }
+    // ).addTo(map);
 
     if (!isLoading) {
       L.heatLayer(heatData, {
